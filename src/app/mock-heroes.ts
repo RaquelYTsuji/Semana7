0502e1d7b929ceb0d2c668464f5c364331c662dc
Raquel Tsuji <raqueltsuji@gmail.com>
2023-05-25 23:34:52 -0300

    Displaying heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Dr. Nice' },
+  { id: 2, name: 'Bombasto' },
+  { id: 3, name: 'Celeritas' },
+  { id: 4, name: 'Magneta' },
+  { id: 5, name: 'RubberMan' },
+  { id: 6, name: 'Dynama' },
+  { id: 7, name: 'Dr. IQ' },
+  { id: 8, name: 'Magma' },
+  { id: 9, name: 'Tornado' }
+];
